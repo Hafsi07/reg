cdcbe261883bd381d0352f860329386e0b37ac99
Hafsi Youssef <hafsiyoussef07@gmail.com>
2025-02-14 22:32:17 +0100

    nuovo file

--- nuovo.r
+++ nuovo.r
@@ -2,7 +2,9 @@ library(ggplot2)
 library(lubridate)
 library(tseries)
 library(forecast)
-
+library(tidyverse)
+library(zoo)
+library(randomForest)
 
 df <- read.csv("C:\\Users\\hafsi\\Downloads\\Electric_Production.csv", stringsAsFactors = FALSE )
 
@@ -27,12 +29,11 @@ ts<- ts(df$IPG2211A2N,start = c(year(min(df$DATE)), month(min(df$DATE))), freque
 
 plot(ts)
 
-autoplot(ts_data) +
-  ggtitle("Electric Production Over Time") +
-  xlab("Year") + ylab("Production") +
-  theme_minimal()
+ts <- diff(ts, differences = 1)  # First differencing
+plot(ts, main = "First Order Differenced Time Series")
 
-adf_test <- adf.test(ts_data)
+
+adf_test <- adf.test(ts)
 print(adf_test)
 
 best_arima <- auto.arima(ts)
@@ -40,7 +41,60 @@ print(best_arima)
 
 forecast_values <- forecast(best_arima, h=12)
 
-autoplot(forecast_values) +
-  ggtitle("Electric Production Forecast") +
-  xlab("Year") + ylab("Production") +
-  theme_minimal()
+plot(forecast_values)
+
+
+######
+data<- df%>% arrange(DATE)
+data$Date <- data $DATE
+
+data$Month <- month(data$Date)
+data$Year <- year(data$Date)
+
+
+data <- data %>%
+  mutate(
+    Lag_1 = lag(IPG2211A2N, 1),
+    Lag_2 = lag(IPG2211A2N, 2),
+    Lag_3 = lag(IPG2211A2N, 3),
+    Rolling_Mean_3 = rollmean(IPG2211A2N, k = 3, fill = NA, align = "right"),
+    Rolling_Mean_6 = rollmean(IPG2211A2N, k = 6, fill = NA, align = "right"),
+    Rolling_Mean_12 = rollmean(IPG2211A2N, k = 12, fill = NA, align = "right")
+  )
+
+data <- na.omit(data)
+
+train_size <- nrow(data) - 12
+train_data <- data[1:train_size,]
+test_data <- data[(train_size + 1):nrow(data),]
+
+predictors <- c("Lag_1", "Lag_2", "Lag_3", "Rolling_Mean_3", "Rolling_Mean_6", "Rolling_Mean_12", "Month", "Year")
+X_train <- train_data[, predictors]
+y_train <- train_data$IPG2211A2N
+X_test <- test_data[, predictors]
+y_test <- test_data$IPG2211A2N
+
+
+set.seed(442200)
+
+rf_model <- randomForest(X_train, y_train, ntree = 100, mtry = 3, importance = TRUE)
+
+test_data$Predicted <- predict(rf_model, X_test)
+
+mae <- abs(mean((test_data$Predicted - test_data$IPG2211A2N)))
+r_squared <- cor(test_data$Predicted, test_data$IPG2211A2N)^2
+
+print(mae)
+print(r_squared)
+
+
+ggplot(test_data, aes(x = Date)) +
+  geom_line(aes(y = IPG2211A2N, color = "Actual"), size = 1) +
+  geom_line(aes(y = Predicted, color = "Predicted"), size = 1, linetype = "dashed") +
+  ggtitle("Actual vs Predicted Electric Production") +
+  ylab("Electric Production") +
+  xlab("Date") +
+  theme_minimal() +
+  scale_color_manual(name = "Legend", values = c("Actual" = "blue", "Predicted" = "red"))
+
+
